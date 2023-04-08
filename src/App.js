a4974eb746373e35fa852d519e232561bfea3915
sagar-442-debug <sagarsapkota0987@gmail.com>
2023-04-08 19:33:50 +0545

    Beginning of the web building

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'tailwindcss/tailwind.css';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className='bg-[#0f1724] h-full w-full absolute -z-10'>
+      </div>
+    <div className='px-10 pt-5'>
+      <Navbar/>
+
     </div>
+    </>
   );
 }
 
