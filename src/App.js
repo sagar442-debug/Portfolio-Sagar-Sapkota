5b92c4be9a4abdb451c03cbd7efe4cdbd841eb30
sagar442-debug <bnradlyp1@gmail.com>
2023-09-29 21:14:30 -0600

    Adding the text and working on responsiveness at the same time also making effort to add image on the right with some kind of animation

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ function App() {
   return (
     <>
       <div className="bg-[#0f1724] h-full w-full absolute -z-10 font-poppins"></div>
-      <div className="px-10 pt-5">
+      <div className="mx-6 pt-5">
         <Navbar />
         <MidSection />
+
+      </div>
+      <div className="circle-setup sm:hidden">
+        <div className="border-2 absolute overflow-hidden border-[#478fce] -z-10 top-24 rounded-full h-[30rem] w-[30rem] animate-pulse  transform -translate-x-1/2">
+        </div>
+        <div className=" absolute pulse bg-[#494397] h-[5rem] w-[5rem] top-[30rem] animate-ping left-20 -z-10 rounded-full ">
+          </div>
       </div>
     </>
   );
--- src/components/MidSection.js
+++ src/components/MidSection.js
@@ -2,24 +2,35 @@ import React from "react";
 
 export default function MidSection() {
   return (
-    <div className="text-white  font-poppins">
-      <div className="text-section mt-10 space-y-3">
-        <h1>
-          <span className="text-[#925ed6] text-3xl font-semibold">
-            Hello, I am Sagar Sapkota,
-          </span>
-        </h1>
-        <h1>
-          <span className="text-[#fff] text-3xl font-semibold">
-            A FullStack developer
-          </span>
-        </h1>
-        <p className="font-normal mt-5">
-        I possess the necessary skills to create basic websites and proficiently handle backend development. My expertise is constantly evolving, as I stay up-to-date with industry trends and advancements.
-        </p>
-        <button className="p-3 bg-[#478fce] rounded-2xl  hover:bg-white hover:text-[#2d3843] duration-500">Download CV</button>
+    <div className="text-white  font-poppins overflow-hidden ">
+      <div className="division-midsection">
+        <div className="text-section mt-20">
+          <h1>
+            <span className="text-[#925ed6] text-3xl font-semibold sm:text-5xl ">
+              Hello, I am Sagar Sapkota,
+            </span>
+          </h1>
+          <h1>
+            <span className="text-[#fff] text-3xl font-semibold sm:text-5xl sm:leading-[2]">
+              A FullStack developer
+            </span>
+          </h1>
+          <p className="font-extralight mt-5 text-lg space-y-5 sm:text-2xl sm:leading-[2]">
+            I possess the necessary skills to create basic websites and
+            proficiently handle backend development. My expertise is constantly
+            evolving, as I stay up-to-date with industry trends and
+            advancements.
+          </p>
+          <div className="button mt-10">
+            <button className="p-3 bg-[#478fce] rounded-2xl hover:bg-white hover:text-[#2d3843] duration-500">
+              Download CV
+            </button>
+          </div>
+        </div>
       </div>
-    
+      <div className="image-section">
+
+      </div>
     </div>
   );
 }
